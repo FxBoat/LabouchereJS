8d68e5e7a868a28758f3ec6fa9366d95cd95013d
wit <fxboat.th@gmail.com>
2022-07-18 22:54:46 +0700

    Updated

--- Labouchere.js
+++ Labouchere.js
@@ -1,64 +1,69 @@
 
 const betting = (result) => {
 
+    if(SUM_NET < TARGET){
+
+        if(result == 'win'){
+            console.log("Betting Win");
+
+            WIN += 1;
+            SUM_NET += BET*PERCENT_WIN; 
+            let remove_last = ARR_STEP.pop();
+            let remove_first = ARR_STEP.shift();
+            
+            let ARR_STEP_length = ARR_STEP.length;
     
-    if(result == 'win'){
-        console.log("Betting Win");
-
+            BET = ARR_STEP[0] + ARR_STEP[ARR_STEP_length-1];
+          
+            console.log(ARR_STEP[ARR_STEP_length-1]);
+            let obj_res = {
+                arr_step : ARR_STEP,
+                // last_bet : LAST_BET,
+                // next_bet : NEXT_BET,
+                bet : BET,
+                sum_net : SUM_NET,
+                win : WIN,
+                lose : LOSE,
+                arr_step_length : ARR_STEP_length
+            }
     
-        WIN += 1;
-        SUM_NET += BET*PERCENT_WIN; 
-        let remove_last = ARR_STEP.pop();
-        let remove_first = ARR_STEP.shift();
-        
-        let ARR_STEP_length = ARR_STEP.length;
-
-        BET = ARR_STEP[0] + ARR_STEP[ARR_STEP_length-1];
-      
-        console.log(ARR_STEP[ARR_STEP_length-1]);
-        let obj_res = {
-            arr_step : ARR_STEP,
-            // last_bet : LAST_BET,
-            // next_bet : NEXT_BET,
-            bet : BET,
-            sum_net : SUM_NET,
-            win : WIN,
-            lose : LOSE,
-            arr_step_length : ARR_STEP_length
+            return obj_res;
+    
+        }else if(result == 'lose'){
+    
+            console.log("Betting Lose");
+            LOSE += 1;
+            SUM_NET += (-BET); 
+            let add_last = ARR_STEP.push(BET);
+            //let add_first = ARR_STEP.unshift(44);
+            let ARR_STEP_length = ARR_STEP.length;
+    
+            BET = ARR_STEP[0] + ARR_STEP[ARR_STEP_length-1];
+    
+            let obj_res = {
+                arr_step : ARR_STEP,
+                // last_bet : LAST_BET,
+                // next_bet : NEXT_BET,
+                bet : BET,
+                sum_net : SUM_NET,
+                win : WIN,
+                lose : LOSE,
+                arr_step_length : ARR_STEP_length
+            }
+    
+            return obj_res;
+    
+    
+        }else{
+            return 'n/a';
         }
-
-        return obj_res;
-
-    }else if(result == 'lose'){
-
-        console.log("Betting Lose");
-       
-        LOSE += 1;
-        SUM_NET += (-BET); 
-        let add_last = ARR_STEP.push(BET);
-        //let add_first = ARR_STEP.unshift(44);
-        let ARR_STEP_length = ARR_STEP.length;
-
-        BET = ARR_STEP[0] + ARR_STEP[ARR_STEP_length-1];
-
-        let obj_res = {
-            arr_step : ARR_STEP,
-            // last_bet : LAST_BET,
-            // next_bet : NEXT_BET,
-            bet : BET,
-            sum_net : SUM_NET,
-            win : WIN,
-            lose : LOSE,
-            arr_step_length : ARR_STEP_length
-        }
-
-        return obj_res;
-
+    
 
     }else{
-        return 'n/a';
+        console.log("Your target complete...");
+        return 
     }
-
+    
 }
 
 module.exports = {
